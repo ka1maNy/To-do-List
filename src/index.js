41050d757aea6598e204a1eea6a4273cb24dc01e
ka1maNy <maxim-amp@mail.ru>
2022-08-08 20:50:51 +0300

    08.08

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -14,4 +14,35 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals();*/
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom/client';
+import Typography from '@material-ui/core/Typography';
+import './index.css';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  return (
+    <div className="App">
+      <Typography component="h1" variant="h2">
+        Список Задач
+      </Typography>
+      <TodoForm saveTodo={todoText => {
+                  const trimmedText = todoText.trim();
+                  if (trimmedText.length > 0) {setTodos(...todos, trimmedText)}
+      }} />
+      <TodoList todos = {todos}/>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+//const rootElement = document.getElementById('root');
+//ReactDOM.render(<App />, rootElement);
