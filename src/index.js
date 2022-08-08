d383d842933da5f53d2fbf14ff8e84b35e3cedc3
ka1maNy <maxim-amp@mail.ru>
2022-08-08 20:54:41 +0300

    Скобки

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ const App = () => {
       </Typography>
       <TodoForm saveTodo={todoText => {
                   const trimmedText = todoText.trim();
-                  if (trimmedText.length > 0) {setTodos(...todos, trimmedText)}
+                  if (trimmedText.length > 0) {setTodos([...todos, trimmedText])}
       }} />
       <TodoList todos = {todos}/>
     </div>
